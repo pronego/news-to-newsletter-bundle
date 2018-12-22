adc8a216cfef63ec32a012cdc45cdd59ad8950b7
Sven Rhinow <sven@sr-tag.de>
2018-12-22 01:14:17 +0100

    Änderungsdatum zur Sortierung, Filter und Suche hinzugefuegt

---
+++ src/Resources/contao/dca/tl_newsletter.php
@@ -0,0 +1,6 @@
+<?php
+$GLOBALS['TL_DCA']['tl_newsletter']['fields']['tstamp']['label'] = &$GLOBALS['TL_LANG']['tl_newsletter']['tstamp'];
+$GLOBALS['TL_DCA']['tl_newsletter']['fields']['tstamp']['filter'] = true;
+$GLOBALS['TL_DCA']['tl_newsletter']['fields']['tstamp']['sorting'] = true;
+$GLOBALS['TL_DCA']['tl_newsletter']['fields']['tstamp']['search'] = true;
+$GLOBALS['TL_DCA']['tl_newsletter']['fields']['tstamp']['flag'] = 8;
