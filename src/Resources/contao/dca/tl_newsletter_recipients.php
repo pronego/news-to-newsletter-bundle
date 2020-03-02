0eb34e65f26b347a9d68a6f4110ca0376c862d04
Jonas Linn <jonas@jlinn.de>
2020-03-02 17:15:18 +0100

    Fix: Felddefinition für zusätzliche Felder bei Newsletterempfängern repariert.

--- src/Resources/contao/dca/tl_newsletter_recipients.php
+++ src/Resources/contao/dca/tl_newsletter_recipients.php
@@ -14,20 +14,20 @@
  */
 $GLOBALS['TL_DCA']['tl_newsletter_recipients']['palettes']['default'] = str_replace
 (
-	'contact',
-	'contact,salutation',
+	'active',
+	'active,salutation',
 	$GLOBALS['TL_DCA']['tl_newsletter_recipients']['palettes']['default']
 );
 $GLOBALS['TL_DCA']['tl_newsletter_recipients']['palettes']['default'] = str_replace
 (
-	'contact',
-	'contact,surname',
+	'active',
+	'active,surname',
 	$GLOBALS['TL_DCA']['tl_newsletter_recipients']['palettes']['default']
 );
 $GLOBALS['TL_DCA']['tl_newsletter_recipients']['palettes']['default'] = str_replace
 (
-	'contact',
-	'contact,firstname',
+	'active',
+	'active,firstname',
 	$GLOBALS['TL_DCA']['tl_newsletter_recipients']['palettes']['default']
 );
 
@@ -40,21 +40,24 @@ $GLOBALS['TL_DCA']['tl_newsletter_recipients']['fields']['salutation'] = array
 	'label'     => &$GLOBALS['TL_LANG']['tl_newsletter_recipients']['salutation'],
 	'exclude'   => true,
 	'inputType' => 'text',
-	'eval'      => array('mandatory'=>true, 'rgxp'=>'digit', 'maxlength'=>8, 'tl_class'=>'w50')
+	'eval'      => array('mandatory'=>true, 'rgxp'=>'alpha', 'maxlength'=>8, 'tl_class'=>'w50'),
+	'sql'		=> "varchar(8) NOT NULL default ''"
 );
 $GLOBALS['TL_DCA']['tl_newsletter_recipients']['fields']['surname'] = array
 (
 	'label'     => &$GLOBALS['TL_LANG']['tl_newsletter_recipients']['surname'],
 	'exclude'   => true,
 	'inputType' => 'text',
-	'eval'      => array('mandatory'=>true, 'rgxp'=>'digit', 'maxlength'=>50, 'tl_class'=>'w50')
+	'eval'      => array('mandatory'=>true, 'rgxp'=>'alpha', 'maxlength'=>50, 'tl_class'=>'w50'),
+	'sql'		=> "varchar(50) NOT NULL default ''"
 );
 $GLOBALS['TL_DCA']['tl_newsletter_recipients']['fields']['firstname'] = array
 (
 	'label'     => &$GLOBALS['TL_LANG']['tl_newsletter_recipients']['firstname'],
 	'exclude'   => true,
 	'inputType' => 'text',
-	'eval'      => array('mandatory'=>true, 'rgxp'=>'digit', 'maxlength'=>50, 'tl_class'=>'w50')
+	'eval'      => array('mandatory'=>true, 'rgxp'=>'alpha', 'maxlength'=>50, 'tl_class'=>'w50'),
+	'sql'		=> "varchar(50) NOT NULL default ''"
 );
 
 
